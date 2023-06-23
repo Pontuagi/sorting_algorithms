cf726988c027f1633659a7af1efa34947c9d5e7a
Pontuagi <benepontuagi@gmail.com>
2023-06-23 11:02:22 +0300

    bubble error corrrection

--- sort.h
+++ sort.h
@@ -2,6 +2,9 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,9 +15,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
