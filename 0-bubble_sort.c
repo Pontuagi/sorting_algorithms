f28bc7c9e6579d45e1b4e409687920b9dfbf4eba
Pontuagi <benepontuagi@gmail.com>
2023-06-23 10:51:57 +0300

    buble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - algorithm to sort an array in ascending order
+ * @array: the array to sort
+ * @size: size of the array
+ *
+ * Return: Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+
+	for (i = 0; i < size -  1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+
+	}
+}
