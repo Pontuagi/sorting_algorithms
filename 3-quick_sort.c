0cdf94fd67f9b8de7086064e9e42b0d32dd1e303
Keith koki <kokikeith@gmail.com>
2023-06-24 05:38:21 -0700

    file quick updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,17 @@
 #include "sort.h"
 
 /**
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
   * lomuto - function to divide an array into two based on pivot. all numbers
   * above pivot on the right and the rest on the left
   * @array: the array to divide
@@ -11,43 +22,49 @@
   */
 int lomuto(int *array, int low, int high)
 {
-	int temp;
 	int pivot = array[high];
 	int i = low - 1;
 	int j;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; ++j)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
-			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			++i;
+			swap(&array[i], &array[j]);
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
+
+	swap(&array[i + 1], &array[high]);
 
 	return (i + 1);
 }
 
 /**
-  * quick_sort - a function to sort an array in ascending order using quick
-  * sort
-  * @array: the array to sort
-  * @size: the size of the array
-  */
+ */
+void quick_sort_helper(int *array, int low, int high)
+{
+	int i, pi;
+
+	if (low < high)
+	{
+		pi = lomuto(array, low, high);
+
+		for (i = low; i <= high; ++i)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+
+		quick_sort_helper(array, low, pi - 1);
+		quick_sort_helper(array, pi + 1, high);
+	}
+}
+
+/**
+ */
 void quick_sort(int *array, size_t size)
 {
-	int pivot_idx;
-
-	if (array == NULL || size <= 1)
-		return;
-
-	pivot_idx = lomuto(array, 0, size - 1);
-	quick_sort(array, pivot_idx);
-	quick_sort(array + pivot_idx + 1, size - pivot_idx - 1);
-
+	quick_sort_helper(array, 0, size - 1);
+	print_array(array, size);
 }
--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int lomuto(int *array, int low, int high);
+void quick_sort_helper(int *array, int low, int high);
 void shell_sort(int *array, size_t size);
 
 #endif
