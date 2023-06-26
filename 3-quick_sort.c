784ee9b6f892675292fba0c2328bf519b0a1651b
Keith koki <kokikeith@gmail.com>
2023-06-26 01:34:13 -0700

    file fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -74,6 +74,9 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size <= 1)
+		return;
+
 	quick_sort_helper(array, 0, size - 1, size);
 	print_array(array, size);
 }
